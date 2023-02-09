3d230321ece4b2abc6788d9607e60cc00e9906e5
Sara <106694716+SaraLi9789@gmail.com.github.com>
2023-02-09 17:47:28 +0100

    Prueba

--- src/SubscripcionTest.java
+++ src/SubscripcionTest.java
@@ -2,11 +2,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class SubscripcionTest {
 
-	@Test
-	void testPrecioPorMes() {
-		fail("Not yet implemented");
-	}
+public class SubscripcionTest {
+@Test
+public void testprecioPorMes() {
+double esperado=1;
+Subscripcion s = new Subscripcion(200,2) ;
+double resultado = s.precioPorMes();
+assertEquals(esperado, resultado,0) ;
+}
 
+@Test
+public void testprecioPorMes2() {
+double esperado=67;
+Subscripcion s = new Subscripcion(200,3) ;
+double resultado= s.precioPorMes();
+assertEquals(esperado, resultado, 0) ;
+}
 }
